c2d81865d8b6cbc55f4917bd99061b5a69caae6b
XiaoJH <doudou0610@gmail.com>
2025-08-20 03:49:15 +0800

    v0.2.0 移动端 长按菜单位置 / 以模态方式弹出设置

--- PhotoViewer/Views/SettingsView.axaml.cs
+++ PhotoViewer/Views/SettingsView.axaml.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Data.Converters;
-using Avalonia.Markup.Xaml;
+﻿using Avalonia.Controls;
 
 namespace PhotoViewer.Views;
 
@@ -14,21 +7,5 @@ public partial class SettingsView : UserControl
     public SettingsView()
     {
         InitializeComponent();
-    }
-}
-
-// 数组转换器（用于显示）
-public class ArrayConverter : IValueConverter
-{
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-    {
-        return value is IEnumerable<double> list 
-            ? string.Join(", ", list.Select(x => x.ToString("P1"))) 
-            : string.Empty;
-    }
-
-    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        throw new NotImplementedException();
     }
 }
