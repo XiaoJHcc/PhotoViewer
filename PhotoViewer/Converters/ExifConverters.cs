3ce36515d2125d0a2331e76e47b0c385fdf74c01
XiaoJH <doudou0610@gmail.com>
2025-09-08 16:43:08 +0800

    EXIF 基础

---
+++ PhotoViewer/Converters/ExifConverters.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using MetadataExtractor;
+
+namespace PhotoViewer.Converters;
+
+/// <summary>
+/// 光圈值转换器
+/// </summary>
+public class ApertureConverter : IValueConverter
+{
+    public static readonly ApertureConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is Rational aperture)
+        {
+            return $"f/{aperture.Numerator/aperture.Denominator}";
+        }
+        return "--";
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
+
+/// <summary>
+/// 快门速度转换器
+/// </summary>
+public class ExposureTimeConverter : IValueConverter
+{
+    public static readonly ExposureTimeConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is Rational exposureTime)
+        {
+            return $"{exposureTime.Numerator}/{exposureTime.Denominator}";
+        }
+        return "--";
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
+
+/// <summary>
+/// 焦距转换器
+/// </summary>
+public class FocalLengthConverter : IValueConverter
+{
+    public static readonly FocalLengthConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is Rational focalLength)
+        {
+            return $"{focalLength.Numerator/focalLength.Denominator}mm";
+        }
+        return "--";
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
+
+/// <summary>
+/// 日期时间转换器
+/// </summary>
+public class DateTimeConverter : IValueConverter
+{
+    public static readonly DateTimeConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is DateTime dateTime)
+        {
+            return dateTime.ToString("yyyy-MM-dd HH:mm");
+        }
+        return "--";
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
+
