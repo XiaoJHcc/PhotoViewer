621420e0b32c3cb954c240cea7468cefe4fba848
XiaoJH <doudou0610@gmail.com>
2025-09-29 14:37:05 +0800

    主窗口UI平台分离

---
+++ PhotoViewer/Windows/SingleView.axaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Avalonia.Controls;
+using PhotoViewer.ViewModels;
+
+namespace PhotoViewer.Windows;
+
+public partial class SingleView : UserControl
+{
+    public SingleView()
+    {
+        InitializeComponent();
+
+        // 监听尺寸变化以检测屏幕方向
+        SizeChanged += OnSizeChanged;
+
+        if (OperatingSystem.IsAndroid() || OperatingSystem.IsIOS())
+        {
+            Loaded += (_, _) =>
+            {
+
+                var insetsManager = TopLevel.GetTopLevel(this).InsetsManager;
+                insetsManager.DisplayEdgeToEdgePreference = true;
+                insetsManager.IsSystemBarVisible = false;
+            };
+        }
+    }
+
+    private void OnSizeChanged(object? sender, SizeChangedEventArgs e)
+    {
+        if (DataContext is MainViewModel viewModel)
+        {
+            viewModel.UpdateScreenOrientation(e.NewSize.Width, e.NewSize.Height);
+        }
+    }
+}
