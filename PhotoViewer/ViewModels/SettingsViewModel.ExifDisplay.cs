3a5b370dcb622f3fb20e102fdfae3a5bd417f56a
XiaoJH <doudou0610@gmail.com>
2025-10-11 01:08:49 +0800

    SettingsViewModel 拆分

---
+++ PhotoViewer/ViewModels/SettingsViewModel.ExifDisplay.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using ReactiveUI;
+using System.Reactive;
+using PhotoViewer.Controls;
+
+namespace PhotoViewer.ViewModels;
+
+public partial class SettingsViewModel
+{
+    //////////////
+    /// EXIF 显示设置
+    //////////////
+    
+    private ObservableCollection<ExifDisplayItem> _exifDisplayItems = new();
+    public ObservableCollection<ExifDisplayItem> ExifDisplayItems
+    {
+        get => _exifDisplayItems;
+        set => this.RaiseAndSetIfChanged(ref _exifDisplayItems, value);
+    }
+
+    private List<ExifDisplayItem> _enabledExifItems = new();
+    public List<ExifDisplayItem> EnabledExifItems
+    {
+        get => _enabledExifItems;
+        private set => this.RaiseAndSetIfChanged(ref _enabledExifItems, value);
+    }
+
+    private void InitializeExifDisplayItems()
+    {
+        ExifDisplayItems = new ObservableCollection<ExifDisplayItem>
+        {
+            new("光圈", "Aperture", true),
+            new("快门", "ExposureTime", true),
+            new("ISO", "Iso", true),
+            new("等效焦距", "EquivFocalLength", true),
+            new("实际焦距", "FocalLength", false),
+            new("相机型号", "CameraModel", false),
+            new("镜头型号", "LensModel", false),
+            new("拍摄时间", "DateTimeOriginal", false),
+            new("曝光补偿", "ExposureBias", false),
+            new("白平衡", "WhiteBalance", false),
+            new("闪光灯", "Flash", false),
+        };
+
+        // 监听集合变化
+        ExifDisplayItems.CollectionChanged += OnExifDisplayItemsChanged;
+        
+        // 为现有项目订阅属性变化
+        foreach (var item in ExifDisplayItems)
+        {
+            item.PropertyChanged += OnExifDisplayItemChanged;
+        }
+
+        UpdateEnabledExifItems();
+    }
+
+    private void OnExifDisplayItemsChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+    {
+        if (e.NewItems != null)
+        {
+            foreach (ExifDisplayItem item in e.NewItems)
+            {
+                item.PropertyChanged += OnExifDisplayItemChanged;
+            }
+        }
+
+        if (e.OldItems != null)
+        {
+            foreach (ExifDisplayItem item in e.OldItems)
+            {
+                item.PropertyChanged -= OnExifDisplayItemChanged;
+            }
+        }
+
+        UpdateEnabledExifItems();
+    }
+
+    private void OnExifDisplayItemChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(ExifDisplayItem.IsEnabled))
+        {
+            UpdateEnabledExifItems();
+        }
+    }
+
+    private void UpdateEnabledExifItems()
+    {
+        EnabledExifItems = ExifDisplayItems
+            .Where(item => item.IsEnabled)
+            .ToList();
+    }
+
+    // 添加移动命令
+    public ReactiveCommand<MoveCommandParameter, Unit> MoveExifDisplayCommand { get; private set; }
+
+    private void OnMoveExifDisplay(MoveCommandParameter parameter)
+    {
+        MoveExifDisplay(parameter.FromIndex, parameter.ToIndex);
+    }
+
+    public void MoveExifDisplay(int fromIndex, int toIndex)
+    {
+        if (fromIndex < 0 || fromIndex >= ExifDisplayItems.Count || 
+            toIndex < 0 || toIndex >= ExifDisplayItems.Count || 
+            fromIndex == toIndex)
+            return;
+
+        var item = ExifDisplayItems[fromIndex];
+        ExifDisplayItems.RemoveAt(fromIndex);
+        ExifDisplayItems.Insert(toIndex, item);
+    }
+
+    public class ExifDisplayItem : ReactiveObject
+    {
+        private string _displayName;
+        private string _propertyName;
+        private bool _isEnabled;
+
+        public string DisplayName
+        {
+            get => _displayName;
+            set => this.RaiseAndSetIfChanged(ref _displayName, value);
+        }
+
+        public string PropertyName
+        {
+            get => _propertyName;
+            set => this.RaiseAndSetIfChanged(ref _propertyName, value);
+        }
+
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+            set => this.RaiseAndSetIfChanged(ref _isEnabled, value);
+        }
+
+        public ExifDisplayItem(string displayName, string propertyName, bool isEnabled = true)
+        {
+            _displayName = displayName;
+            _propertyName = propertyName;
+            _isEnabled = isEnabled;
+        }
+    }
+}
+
---
+++ PhotoViewer/ViewModels/SettingsViewModel.ScalePresets.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using ReactiveUI;
+
+namespace PhotoViewer.ViewModels;
+
+public partial class SettingsViewModel
+{
+    //////////////
+    /// 缩放倍率预设
+    //////////////
+        
+    private ObservableCollection<ScalePreset> _scalePresets = [
+        new("12.5"), 
+        new("25"), 
+        new("33.333"),
+        new("50"),
+        new("75"),
+        new("100"),
+        new("200"),
+        new("400")
+    ];
+    public ObservableCollection<ScalePreset> ScalePresets
+    {
+        get => _scalePresets;
+        set => this.RaiseAndSetIfChanged(ref _scalePresets, value);
+    }
+    // 添加预设 +
+    public void AddScalePreset() => ScalePresets.Add(new("100"));
+    // 删除预设 -
+    public void RemoveScalePreset(ScalePreset item) => ScalePresets.Remove(item);
+    // 排序预设 回车或失焦后
+    public void SortScalePreset()
+    {
+        var sorted = ScalePresets.OrderBy(x => x.Value).ToList();
+        ScalePresets = new ObservableCollection<ScalePreset>(sorted);
+    }
+    // 切换编辑模式
+    public void EditScalePreset(ScalePreset item)
+    {
+        item.Editing = true;
+    }
+    // 应用编辑
+    public void ApplyScalePreset()
+    {
+        foreach (var preset in ScalePresets)
+        {
+            preset.Editing = false;
+        }
+        SortScalePreset();
+    }
+
+    public class ScalePreset : ReactiveObject
+    {
+        private double _value;
+        public double Value
+        {
+            get => _value;
+            set
+            {
+                Display = value.ToString("P1");
+                this.RaiseAndSetIfChanged(ref _value, value);
+            }
+        }
+
+        private string _text;
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                double newValue;
+                if (double.TryParse(value, out newValue))
+                {
+                    Value = newValue / 100;
+                    this.RaiseAndSetIfChanged(ref _text, value);
+                }
+                else
+                {
+                    Display = "Error";
+                    this.RaiseAndSetIfChanged(ref _text, value);
+                }
+            }
+        }
+
+        private string _display;
+        public string Display
+        {
+            get => _display;
+            private set => this.RaiseAndSetIfChanged(ref _display, value);
+        }
+
+        private bool _editing = false;
+
+        public bool Editing
+        {
+            get => _editing;
+            set => this.RaiseAndSetIfChanged(ref _editing, value);
+        }
+
+        public ScalePreset(string text)
+        {
+            Text = text;
+        }
+    }
+}
+
