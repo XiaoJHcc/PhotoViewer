103e28efc4728c03c0d5fd9443bcc19395834f68
XiaoJH <doudou0610@gmail.com>
2025-10-12 15:28:33 +0800

    设置 UI 优化

---
+++ PhotoViewer/Views/Settings/ExifSettingsView.axaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using PhotoViewer.Controls;
+using PhotoViewer.ViewModels;
+using Avalonia.Data.Converters;
+
+namespace PhotoViewer.Views;
+
+public partial class ExifSettingsView : UserControl
+{
+    public ExifSettingsView()
+    {
+        InitializeComponent();
+    }
+}
